5a4b639ed58064931f554e92ed8bfb746c917e51
Kelvin Wu <wu.kelvin91@gmail.com>
2022-03-23 13:40:43 +0800

    First Commit (migration)

---
+++ index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const axios = require("axios");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const { uuid } = require("uuidv4");
+
+const PORT = process.env.SERVER_PORT || 2000;
+
+app.use(express.json());
+app.use("/static", express.static("assets"));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+if (process.env.NODE_ENV === "development") {
+  console.log("Morgan enabled");
+  app.use(morgan("common"));
+}
+
+app.get("/", (req, res) => {
+  res.send("Nothing here");
+});
+
+app.get("/api/healthcheck", (req, res) => {
+  res.send("Service is healthy");
+});
+
+app.get("/api/all-pokemons-gen1", async (req, res) => {
+  const api_url = process.env.POKEMON_API_URL + "?limit=151";
+  const pokemons = await axios.get(api_url);
+  const results = pokemons.data.results;
+  res.send(results);
+});
+
+app.get("/api/catchem-all-10", async (req, res) => {
+  const api_url = process.env.POKEMON_API_URL + "?limit=151";
+  const pokemons = await axios.get(api_url);
+  const results = pokemons.data.results;
+  const randomPokemons = [];
+
+  for (let i = 0; i < 10; i++) {
+    const random = generateRandomNumber(151);
+    const newResult = { ...results[random] };
+    newResult.id = uuid();
+    randomPokemons.push(newResult);
+  }
+  console.log(randomPokemons);
+  res.send(randomPokemons);
+});
+
+function generateRandomNumber(totalPokemons) {
+  return Math.ceil(Math.random() * totalPokemons);
+}
+
+app.listen(PORT, () => console.log("Server is up and running at port", PORT));
