318668a7611fa46aacf84ade7a39ef3bb07246bb
Kelvin Wu <wu.kelvin91@gmail.com>
2022-03-28 00:38:18 +0800

    update rate

--- gacha.js
+++ gacha.js
@@ -24,7 +24,7 @@ const allPokemons = _.groupBy(
 
 // Gacha Rates
 const GACHA_RATES = {
-  UR: 0.005, // 0.5%
+  UR: 0.5, // 0.5%
   SSR: 0.0375, // 3.75%
   SR: 0.1575, // 15.75%
   R: 0.8, // 80%
